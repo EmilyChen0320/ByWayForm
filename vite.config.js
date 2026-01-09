26eab98ce9c3787e18114df3e16cdad92960d1dd
EmilyChen0320 <emily320@aicreate360.com>
2026-01-09 14:35:50 +0800

    base: '/',

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         vue(),
     ],
-    base: '/ByWay/',
+    base: '/',
     resolve: {
         alias: {
             '@': resolve(__dirname, 'resources/js'),
